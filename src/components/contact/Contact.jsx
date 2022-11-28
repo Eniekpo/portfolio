60a7bf690f56dbcd0840bd5b187324f76040279e
Eniekpo <johneniekpo4life@gmail.com>
2022-11-28 22:36:51 +0100

    a

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,48 @@
 import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
 
 const Contact = () => {
   return (
     <section id='contact'>
-      Contact
-      </section>
+      <h5>Get In Touch</h5>
+      <h2>CONTACT ME</h2>
+
+      <div className="container contact-container">
+        <div className="contact-options">
+
+          <article className="contact-option">
+            <MdOutlineEmail className='contact-option-icon'/>
+            <h4>Email</h4>
+            <h5>johneniekpo@outlook.com</h5>
+            <a href="mailto:johneniekpo@outlook.com" target='_blank' rel='noreferrer noopener'>Contact Me</a>
+          </article>
+
+          <article className="contact-option">
+            <RiMessengerLine className='contact-option-icon' />
+            <h4>Messenger</h4>
+            <a href="https://m.me/Eniekpo" target='_blank' rel='noreferrer noopener'>Contact Me</a>
+          </article>
+
+          <article className="contact-option">
+            <BsWhatsapp className='contact-option-icon' />
+            <h4>WhatsApp</h4>
+            <a href="https://wa.me/+2347032767758" target='_blank' rel='noreferrer noopener'>Contact Me</a>
+          </article>
+
+        </div>
+
+        {/* END OF CONTACT OPTIONS  */}
+
+        <form action="">
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="2" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,32 @@
 import './footer.css'
+import {FaFacebook, FaInstagram, FaTwitter, FaLinkedin} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#home" className='footer-logo'>YOLE-TECH</a>
+
+      <ul className='permalinks'>
+        <li><a href="#home">HOME</a></li>
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#experience">EXPERIENCE</a></li>
+        <li><a href="#services">SERVICES</a></li>
+        <li><a href="#portfolio">PORTFOLIO</a></li>
+        <li><a href="#testimonials">TESTIMONIALS</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+
+      <div className="footer-socials">
+        <a href="https://facebook.com/Eniekpo" target='_blank' rel='noreferrer noopener'><FaFacebook/></a>
+        <a href="https://twitter.com/Eniekpo" target='_blank' rel='noreferrer noopener'><FaTwitter/></a>
+        <a href="https://instagram.com/Eniekpo" target='_blank' rel='noreferrer noopener'><FaInstagram/></a>
+        <a href="https://linkedin.com/in/Eniekpo" target='_blank' rel='noreferrer noopener'><FaLinkedin/></a>
+      </div>
+
+      <div className="footer-copyright">
+        <small>Copyright &copy; Yole-Tech 2022, All Rights Reserved</small>
+      </div>
+    </footer>
   )
 }
 
